1e4fa541d340368813263a8ad50892cc1ada7f6b
Ian Henriksen <insertinterestingnamehere@gmail.com>
2019-01-31 15:15:14 -0600

    Stop using conda's fake cross-compile stuff for now.

--- conda_recipe_galois/build.sh
+++ conda_recipe_galois/build.sh
@@ -1,8 +1,8 @@
 rm -rf build
 mkdir build
 cd build
-CMAKE_PLATFORM_FLAGS+=(-DCMAKE_TOOLCHAIN_FILE="${RECIPE_DIR}/cross-linux.cmake")
-cmake -DCMAKE_BUILD_TYPOE=Release -DCMAKE_VERBOSE_MAKEFILE=ON -DCMAKE_CXX_FLAGS="-I/usr/include -L/usr/lib64 -fPIC" -DCMAKE_INSTALL_PREFIX=$BUILD_PREFIX -DBOOST_ROOT=$BUILD_PREFIX ${CMAKE_PLATFORM_FLAGS[@]} ..
+#CMAKE_PLATFORM_FLAGS+=(-DCMAKE_TOOLCHAIN_FILE="${RECIPE_DIR}/cross-linux.cmake")
+cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_VERBOSE_MAKEFILE=ON -DCMAKE_CXX_FLAGS="-I/usr/include -L/usr/lib64 -fPIC" -DCMAKE_INSTALL_PREFIX=$PREFIX -DBOOST_ROOT=$BUILD_PREFIX ..
 make galois_shmem -j
 # Manually install runtime static lib.
 cp libgalois/libgalois_shmem.a $PREFIX/lib/libgalois_shmem.a
