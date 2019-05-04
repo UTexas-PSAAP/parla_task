28b320e14d616bf29450c9962c0c3d88ae9879f6
Ian Henriksen <insertinterestingnamehere@gmail.com>
2019-05-04 13:22:45 -0500

    Use latest galois in recipe. Also build a shared library.

--- conda_recipe_galois/build.sh
+++ conda_recipe_galois/build.sh
@@ -2,9 +2,10 @@ rm -rf build
 mkdir build
 cd build
 #CMAKE_PLATFORM_FLAGS+=(-DCMAKE_TOOLCHAIN_FILE="${RECIPE_DIR}/cross-linux.cmake")
-cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_VERBOSE_MAKEFILE=ON -DCMAKE_CXX_FLAGS="-I/usr/include -L/usr/lib64 -fPIC" -DCMAKE_INSTALL_PREFIX=$PREFIX -DBOOST_ROOT=$BUILD_PREFIX ..
+cmake -DBUILD_SHARED_LIBS=ON -DCMAKE_BUILD_TYPE=Release -DCMAKE_VERBOSE_MAKEFILE=ON -DCMAKE_CXX_FLAGS="-I/usr/include -L/usr/lib64 -fPIC" -DCMAKE_INSTALL_PREFIX=$PREFIX -DBOOST_ROOT=$BUILD_PREFIX ..
 make galois_shmem -j
+make install
 # Manually install runtime static lib.
-cp libgalois/libgalois_shmem.a $PREFIX/lib/libgalois_shmem.a
-mkdir -p $PREFIX/include
-cp -r ../libgalois/include/galois $PREFIX/include/galois
+#cp libgalois/libgalois_shmem.a $PREFIX/lib/libgalois_shmem.a
+#mkdir -p $PREFIX/include
+#cp -r ../libgalois/include/galois $PREFIX/include/galois
